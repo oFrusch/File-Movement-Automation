4229287c17ff93e2caf292f51ae2e2e339c2a010
Owen Carpenter <owenc343@gmail.com>
2018-06-27 09:40:55 -0400

    First Working Version

---
+++ automate_move_files.ps1
@@ -0,0 +1,12 @@
+﻿function move_items{
+    Param($source, $dest, $extension)
+    $new_dir = -join("PDFS_", (Get-Date).ToString('MM-dd-yyyy'))
+    $new_path = Join-Path $dest $new_dir
+    if(!(Test-Path $new_path)){
+        New-Item -Force -Path $new_path -ItemType "directory"
+    }
+    $items = Join-Path $source $extension
+    Move-Item -Path $items -Destination $new_path 
+}
+
+    move_items "C:\Users\ocarpent\Downloads" "C:\Users\ocarpent\Documents\PDF" "*.pdf"
