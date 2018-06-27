ed8312c0299528bac69faf9aa68aa60d51df98ad
Owen Carpenter <owenc343@gmail.com>
2018-06-27 09:52:36 -0400

    Added user prompts for source and movement destinations, as well as file type.

--- automate_move_files.ps1
+++ automate_move_files.ps1
@@ -1,12 +1,14 @@
-﻿function move_items{
-    Param($source, $dest, $extension)
-    $new_dir = -join("PDFS_", (Get-Date).ToString('MM-dd-yyyy'))
-    $new_path = Join-Path $dest $new_dir
-    if(!(Test-Path $new_path)){
-        New-Item -Force -Path $new_path -ItemType "directory"
-    }
-    $items = Join-Path $source $extension
-    Move-Item -Path $items -Destination $new_path 
-}
+﻿$source = Read-Host "Enter source destination of files."
+$dest = Read-Host "Enter movement destination of files."
+$extension = Read-Host "Enter file extension type. If you would like to get all files of that type enter '*.filetype'."
 
-    move_items "C:\Users\ocarpent\Downloads" "C:\Users\ocarpent\Documents\PDF" "*.pdf"
+$new_dir = -join("PDFS_", (Get-Date).ToString('MM-dd-yyyy'))
+$new_path = Join-Path $dest $new_dir
+if(!(Test-Path $new_path)){
+    New-Item -Force -Path $new_path -ItemType "directory"
+}
+$items = Join-Path $source $extension
+Move-Item -Path $items -Destination $new_path 
+
+
+# move_items "C:\Users\ocarpent\Downloads" "C:\Users\ocarpent\Documents\PDF" "*.pdf"
